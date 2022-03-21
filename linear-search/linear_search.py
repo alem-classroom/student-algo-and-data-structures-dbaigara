4bd60cd5239d9096ab09e09a2b96d998a9a85dae
dbaigara_desktop <dbaigara@cern.ch>
2022-03-21 11:50:24 +0300

    done linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,8 @@
 def linear_search(lst, to_find):
+  if to_find in lst:
+    return to_find
+  else:
+    return -1
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
